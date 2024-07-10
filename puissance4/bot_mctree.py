12c221f193fc06056d01b5894ca5a3e77feb5622
dmitrii_D <dmitrii.druzhbin@le-campus-numerique.fr>
2024-07-10 15:08:41 +0200

    working on a MCtreeNode

---
+++ puissance4/bot_mctree.py
@@ -0,0 +1,65 @@
+import random
+import math
+from copy import deepcopy
+from board import Board
+from victory import Victory
+
+class MCtreeNode:
+
+    def __init__(self, board, parent=None, move=None):
+        self.board = board
+        self.parent = parent
+        self.move = move
+        self.children = []
+        self.wins = 0
+        self.visits = 0
+        self.untried_moves = [col for col in range(7) if self.board[0][col] == '   ']
+
+    def select_child(self):
+        best_child = None
+        best_value = - 0.01
+        for child in self.children:
+            ucb1_value = (child.wins / child.visits) + math.sqrt(2 * math.log(self.visits) / child.visits)
+            if ucb1_value > best_value:
+                best_value = ucb1_value
+                best_child = child
+        return best_child
+    
+    def expand(self):
+        move = self.untried_moves.pop()
+        new_board = deepcopy(self.board)
+        new_board.place_symbol('x' if new_board.get_valid_moves().count() % 2 == 0 else 'o', move)
+        child_node = MCtreeNode(new_board, parent=self, move=move)
+        self.children.append(child_node)
+        return child_node
+    
+    def check_winner(self,board):
+        win = Victory(board)
+        if win.check_victory('x'):
+            return 'x'
+        elif win.check_victory('o'):
+            return 'o'
+        else:
+            return None
+    
+    def simulate(self):
+        current_board = deepcopy(self.board)
+        current_symbol = 'x' if current_board.get_valid_moves().count() % 2 == 0 else 'o'
+        while current_board.get_winner() is None:
+            valid_moves = current_board.get_valid_moves()
+            move = random.choice(valid_moves)
+            current_board.place_symbol(current_symbol, move)
+            current_symbol = 'o' if current_symbol == 'x' else 'x'
+        winner = current_board.get_winner()
+        if winner == 'x':
+            return 1
+        elif winner == 'o':
+            return -1
+        else:
+            return 0
+    
+    def backpropagate(self, result):
+        self.visits += 1
+        self.wins += result
+        if self.parent:
+            self.parent.backpropagate(result)
