a68ce8f663c5ab86fd05806d8fb832615a31ad3d
Léo CHAUSSY <chaussyleo@gmail.com>
2024-07-10 16:26:50 +0200

    change player class for minmax

--- puissance4/ai_player.py
+++ puissance4/ai_player.py
@@ -3,18 +3,16 @@ from puissance4.player import Player
 
 class AI_player(Player):
 
-    def __init__(self, name, symbol, board: Board):
+    def __init__(self, name, symbol, board: Board, depth=2):
         super().__init__(name, symbol, board)
+        self.depth = depth
 
     def play(self):
-        #super().play()
-        
-        #No function game_over is defined(fill the whole board to fix)
 
-        pass
-        #column = input(f"{self.name}, in which column do you want to add your token ? ")
-        #try:
-        #    self.board.place(self.symbol, column)
-        #except ValueError:
-        #    print("Choose another column, this column is full ! ")
-        #self.board.display() 
+        self.minmax(depth=self.depth)
+        self.board.display() 
+
+    def minmax(self, depth = 2):
+        if super.can_play() or depth == 0:
+
+
