c8cf0cb3bd9dd4788ceb62268f42115a31e8a398
Léo CHAUSSY <chaussyleo@gmail.com>
2024-07-10 16:55:03 +0200

    continuing min max

--- puissance4/ai_player.py
+++ puissance4/ai_player.py
@@ -1,5 +1,6 @@
 from puissance4.board import Board
 from puissance4.player import Player
+import copy
 
 class AI_player(Player):
 
@@ -13,6 +14,32 @@ class AI_player(Player):
         self.board.display() 
 
     def minmax(self, depth = 2):
-        if super.can_play() or depth == 0:
+        bestscore = 0
+        bestmove = 0
+        noeud = 0
+        score =  0
 
+        if super.can_play() or depth == 0:
+            return bestmove
+        #MAX
+        if noeud == 3:
+            bestscore = -10000
+            for column in range(6):
+                virtual_board = copy.deepcopy(self.board.board)
+                self.board.place(self.symbol, column)
+                score = self.minmax(depth = depth-1)
+                self.board.board = virtual_board
+
+                if score > bestscore:
+                    bestscore = score
+                    bestmove = column
+        #MIN
+        else:
+            pass
+
+
+               
+            
+
+            
 
