88c112592fdbafdc898aca67bd9c04bcbf43c5ba
Léo CHAUSSY <chaussyleo@gmail.com>
2024-07-11 10:50:24 +0200

    added minmax function, score functions to victory class, still not functional

--- puissance4/ai_player.py
+++ puissance4/ai_player.py
@@ -1,5 +1,7 @@
 from puissance4.board import Board
 from puissance4.player import Player
+from puissance4.victory import Victory
+
 import copy
 
 class AI_player(Player):
@@ -9,33 +11,63 @@ class AI_player(Player):
         self.depth = depth
 
     def play(self):
-
-        self.minmax(depth=self.depth)
+        available_moves = {}
+        for move in range(6):
+            score = self.minmax(move, self.symbol, depth=self.depth)
+            available_moves[move] = score
+        bestmove = max(available_moves, key=available_moves.get)
+        self.board.place(self.symbol, bestmove)
         self.board.display() 
 
-    def minmax(self, depth = 2):
+    def minmax(self, move, symbol, depth = 2):
         bestscore = 0
         bestmove = 0
         noeud = 0
         score =  0
+        
+        vic_ob = Victory(self.board.board)
 
-        if super.can_play() or depth == 0:
-            return bestmove
+        if (not super().can_play()) or depth == 0:
+            bestscore = vic_ob.check_move(self.symbol)
+            return bestscore
+
         #MAX
-        if noeud == 3:
+        if (noeud % 2) == 0:
             bestscore = -10000
             for column in range(6):
-                virtual_board = copy.deepcopy(self.board.board)
-                self.board.place(self.symbol, column)
-                score = self.minmax(depth = depth-1)
-                self.board.board = virtual_board
+                board_copy = copy.deepcopy(self.board.board)
+                try:
+                    self.board.place(self.symbol, column)
+                    score = self.minmax(column, self.symbol, depth = depth-1)
+                    self.board.board = board_copy
+                    noeud += 1
+                    depth -= 1 
+                    if score > bestscore:
+                        bestscore = score
+                        bestmove = column
+                except ValueError:
+                    continue
 
-                if score > bestscore:
-                    bestscore = score
-                    bestmove = column
         #MIN
-        else:
-            pass
+        elif (noeud % 2) == 1:
+            bestscore = +10000
+            for column in range(6):
+                try:
+                    board_copy = copy.deepcopy(self.board.board)
+                    #Play for other player
+                    other_symbol = "".join({"x","o"} - set(self.symbol))
+                    self.board.place(other_symbol, column)
+                    score = self.minmax(column, other_symbol, depth = depth-1)
+                    self.board.board = board_copy
+                    noeud += 1
+                    depth -= 1 
+                    if score < bestscore: 
+                        bestscore = score
+                        bestmove = column
+                except ValueError:
+                    continue
+
+        return bestscore
 
 
                
--- puissance4/victory.py
+++ puissance4/victory.py
@@ -1,5 +1,6 @@
 from puissance4.board import Board
 from functools import partial
+from icecream import ic
 
 class Victory:
 
@@ -13,8 +14,6 @@ class Victory:
         return kernel
         
     def check_kernel(self, kernel, symbol):
-        cpt = 0
-
         #Check rows
         for row in kernel:
             if set(row) == set(symbol):
@@ -36,7 +35,62 @@ class Victory:
             return True
         else:
             return False
-         
+    
+    def check_vector(self, vector, symbol):
+        '''Check the return score of a given vector of the kernel'''
+        twos = 50
+        threes = 500
+        fours = 4000
+        score = 0
+        cpt = 0
+
+        for elt in vector:
+            if elt == symbol:
+                cpt += 1
+        if cpt == 2:
+            score += twos
+        elif cpt == 3:
+            score += threes
+        elif cpt == 4:
+            score += fours
+
+        return score
+    
+    def check_score_kernel(self, kernel, symbol):
+        """ Alternatively, check the 'score' of a kernel, associated with number of 2, 3, or 4 symbols in a row """
+        score = 0
+        
+        #Check rows
+        for row in kernel:
+            ic(row, symbol)
+            score += self.check_vector(row, symbol)
+
+        #Check columns
+        for i in range(4):
+            column = [row[i] for row in kernel]
+            score += self.check_vector(column, symbol)
+
+        #Check diagonals
+        diag1 = [kernel[i][-(i+1)] for i in range(len(kernel))]
+        score += self.check_vector(diag1, symbol)
+
+        diag2 = [kernel[i][i] for i in range(len(kernel))]
+        score += self.check_vector(diag2, symbol)
+
+        return score
+ 
+
+    def check_move(self, symbol):
+        score = 0
+        for i in range(4):
+            for j in range(4,8):
+                #Lots of useless computations here (change stride of kernel)
+                ker = self.victory_kernel(i, j)
+                score += self.check_score_kernel(ker, symbol)
+        return score
+
+
+
     def check_victory(self, symbol):
         win = False
         for i in range(4):
